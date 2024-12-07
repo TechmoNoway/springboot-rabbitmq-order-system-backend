02d150c00d6ed052d56789362c6b959871d69c4f
TechmoNoway <cuty0406@outlook.com>
2024-12-07 15:43:39 +0700

    2: Setup source base

---
+++ src/test/java/com/trickynguci/springbootrabbitmqordersystembackend/SpringbootRabbitmqOrderSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trickynguci.springbootrabbitmqordersystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRabbitmqOrderSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
